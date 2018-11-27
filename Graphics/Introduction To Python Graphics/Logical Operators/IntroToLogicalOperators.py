f17677b91d80ded926bf169f4883d859ba80c7ea
James <39444003+magicalsoup@users.noreply.github.com>
2018-11-27 14:56:29 -0500

    Update IntroToLogicalOperators.py

--- Graphics/Introduction To Python Graphics/Logical Operators/IntroToLogicalOperators.py
+++ Graphics/Introduction To Python Graphics/Logical Operators/IntroToLogicalOperators.py
@@ -1,5 +1,5 @@
 import turtle
-
+'''
 x = False
 
 if not x:
@@ -29,3 +29,26 @@ if 1 > 2 and 4 < 10:
 
 if 4 < 10 or 1 < 2:
     print("condition met")
+
+'''
+# Exercise 3
+
+def not_in_box():
+    if turtle.xcor() > 100 or turtle.ycor() > 100
+    or turtle.xcor() < -100 or turtle.ycor() < -100:
+        return True
+    return False
+
+
+def forward(length):
+    while length > 0:
+        if not_in_box():
+            angle = turtle.towards(0, 0)
+            turtle.setheading(angle)
+        turtle.forward(1)
+        length = length - 1
+
+
+turtle.left(45)
+turtle.forward(200)
+forward(200)
